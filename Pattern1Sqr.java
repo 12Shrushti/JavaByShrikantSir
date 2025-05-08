ea0a9ba5de4f60eb8945c0128c232cae68425d11
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-05-09 00:46:31 +0530

    Pattern1Sqr.java

---
+++ Pattern1Sqr.java
@@ -0,0 +1,20 @@
+class Pattern1Sqr{
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++){
+			for(int j=0; j<5; j++){
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
+
+
+ 
+ // *  *  *  *  *
+ // *  *  *  *  *
+ // *  *  *  *  *
+ // *  *  *  *  *
+ // *  *  *  *  *
