e88ee4cc30400e617bbd924659c499f905e2167f
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-05-13 23:49:12 +0530

    CogniizantPractice2.java

---
+++ cognizantPractice2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class cognizantPractice2{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int n= sc.nextInt();
+
+		if(n!=0){
+			if(n<1 || n>10){
+			continue;
+			System.out.println(n);
+		}
+		}
+
+
+		
+	}
+	
+}
