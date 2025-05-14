83e5e3d30c5c5e00c8be10095d90b89fbe1b229c
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-05-15 00:36:31 +0530

    smallInArray.java

---
+++ smallInArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class smallInArray{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// System.out.print("Enter the number: ");
+		int []arr={25,60,91,45,36,70};
+		int small=arr[0];
+
+		for(int i=0;i<=arr.length-1; i++){
+			if(arr[i]<small){
+				small=arr[i];
+			}
+		}
+		System.out.println(small);
+	}
+}
