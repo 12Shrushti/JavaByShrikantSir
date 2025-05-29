ce95893f797abae85761db0038b1cb515241f772
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-05-29 13:44:04 +0530

    TcsPrep1.java

---
+++ TcsPrep1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class TcsPrep1{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int num=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=num;i++){
+			if(num%i==0){
+				System.out.print(i+" ");
+			}
+		}
+		
+	}
+}
