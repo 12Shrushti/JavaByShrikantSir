c78b0ae83038074366b9a0e6bfd19d8e33890d92
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-01 18:58:06 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,28 @@
+class Prime{
+public static void main(String[] args) {
+	int num=20;
+	int start =1;
+	int end =num;
+	
+	
+	for(int i=start;i<=end; i++){
+		if(isPrime(i))
+			System.out.println(i+" ");
+
+	}
+
+	
+}
+public static boolean isPrime(int num){
+	
+	int cnt=0;
+		for(int i=1; i<=num;i++)
+		if(num%i==0)
+			cnt++;
+	if(cnt>2)
+		return false;
+
+	return true;
+
+}
+}
