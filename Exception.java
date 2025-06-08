024de5696b905347c76ee63c3359885120869d7c
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-09 02:11:28 +0530

    Exception.java

---
+++ Exception.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Exception{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first Number: ");
+		int a=sc.nextInt();
+		System.out.print("Enter the Second Number: ");
+		int b=sc.nextInt();
+		System.out.print("Answer: ");
+		try{
+			System.out.println(a/b);
+		}
+		catch(ArithmeticException e){
+			System.out.println("DO not divide number with zero");
+		}
+	}
+}
