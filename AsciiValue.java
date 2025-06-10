3ca838c1c05d6acf0ca40f3733284679d9fd19f1
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-10 12:26:26 +0530

    AsciiValue.java

--- AsciiValue.java
+++ AsciiValue.java
@@ -1,7 +1,9 @@
 class AsciiValue{
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		int ch=65;;
-		for(char i ='A';i<='Z';i++,ch++){
+		for(char i ='A';i<='Z';i++,ch++)
+		{
 			System.out.println(i+":"+ch);
 		}
 	}
