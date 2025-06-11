0f18766a6c0c7fe1b95f23439ce217f35735d671
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-11 23:47:41 +0530

    FrequencyNum.java

---
+++ FrequencyNum.java
@@ -0,0 +1,9 @@
+class FrequencyNum{
+	public static void main(String[] args) {
+		int arr[] = {1,2,4,5,7,2,1,3,5,6,1,2,1};
+		Arrays.sort(arr);
+		for(int i =0; i<=arr.length(); i++){
+			
+		}
+	}
+}
