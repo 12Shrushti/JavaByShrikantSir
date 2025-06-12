543485a0f6b38388f91e06a394cfb4171372feb1
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-13 00:27:17 +0530

    Asc.java

---
+++ Asc.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Asc{
+	public static void main(String[] args) {
+		int [] arr={23,78,43,21,11,88,54,34};
+		Arrays.sort(arr);
+
+		
+		for(int i=0;i<=arr.length-1; i++){
+			System.out.print(arr[i]+" ");	
+		}
+		
+		
+
+	}
+}
