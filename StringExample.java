7643ce43dc3e0eabd6900f1ef6eb9a5f71f16e7e
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2025-06-15 16:31:19 +0530

    StringExample.java

--- StringExample.java
+++ StringExample.java
@@ -1,5 +1,14 @@
 class StringExample{
 	public static void main(String[] args) {
+		String str= "Hello";
+		String str2= "";
+		
+		for(int i= str.length()-1; i>=0;i--){
+			str2 += str.charAt(i);
+			
+		}
+		System.out.print(str2);
+
 		
 	}
 }
